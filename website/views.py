5f169b350b83012332b3a0dc22634b56cc2023a2
Shakeeb ur Rehman <shakeeburrehman78@gmail.com>
2025-07-25 09:46:04 +0500

    Refactor Templates: Create base.html and Extend index.html & blog.html

--- website/views.py
+++ website/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_page(request):
+    return render(request,'website/index.html')
+
+
+def blog_page(request):
+    return render(request,'website/blog.html')
