a301c1312b2a55fcabf8bfbcae14b06764083e7d
Shakeeb ur Rehman <shakeeburrehman78@gmail.com>
2025-07-26 10:35:14 +0500

    Resolved github copilot reviews

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home_page,name='home'),
-    path('blog/',views.blog_page,name='blog')
+    path('', views.home_page,name='home'),
+    path('blog/', views.blog_page,name='blog')
 ]
